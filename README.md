14d2fd661fa61fede89979405c1ad82966df291b
Thomas Sechrist <tsechrist@mail.sfsu.edu>
2019-03-12 13:26:12 -0700

    getting input

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # CSC 415 - Project 3 - My Shell
 
-## Student Name: Name here
+## Student Name: Thomas Sechrist
 
-## Student ID: ID Here
+## Student ID: 918679091
 
 ## Build Instructions
 
--- myshell.c
+++ myshell.c
@@ -1,5 +1,5 @@
 /****************************************************************
- * Name        :                                                *
+ * Name        :  Thomas Sechrist                               *
  * Class       :  CSC 415                                       *
  * Date        :                                                *
  * Description :  Writting a simple bash shell program          *
@@ -23,10 +23,106 @@
 #define PROMPT "myShell >> "
 #define PROMPTSIZE sizeof(PROMPT)
 
-int 
-main(int* argc, char** argv)
+int
+main(int argc, char** argv)
 {
 
-    
+
+    int myargc;
+
+    char myargv[256];
+    char input[256];
+
+
+    while(strcmp(input, "exit"))
+    {
+
+        gets(input);
+        int count = 1;
+        printf("input: %s\n",  input);
+        printf("strlen(input): %d\n", strlen(input));
+
+        // Returns first token
+        char* token = strtok(input, " ");
+
+        // Keep printing tokens while one of the
+        // delimiters present in str[].
+        for(int i = 0; token != NULL; i++)
+        {
+            printf("token: %s\n", token);
+            myargv[i] = token;
+            printf("myargv[%d]: %s\n", i, &myargv[i]);
+            token = strtok(NULL, " ");
+        }
+
+//        printf("myargv: %s\n", myargv[0]);
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+    //int execvp
+
+//    pid_t id;
+//    int pipe_fd[2]; //0 read end and 1 write end
+//
+//    char* left_side[] = {"ls", "-l", "/", NULL};
+//    char* right_side[] = {"wc", "-l", NULL};
+//    char* right_right_side[] = {"wc", "-c", NULL};
+//
+//    pipe(pipe_fd);
+//
+//    id = fork();
+//    if(id == 0) {
+//        close(0);
+//        dup(pipe_fd[0]);
+//        close(pipe_fd[0]);
+//        close(pipe_fd[1]);
+//        execvp(right_side[0], right_side);
+//    }
+//
+//    id = fork();
+//    if(id == 0) {
+//        close(1);
+//        dup(pipe_fd[1]);
+//        close(pipe_fd[0]);
+//        close(pipe_fd[1]);
+//        execvp(left_side[0], left_side);
+//    }
+//    wait(0);
+//
+////    id = fork();
+//
+//    if(id > 0)
+//    {
+//        printf("Parent: %d\n", getpid());
+//        wait(0);
+//
+//    }
+//    else if(id == 0)
+//    {
+//        printf("Child: %d\n", getpid());
+//        execlp("ls", "ls", NULL);
+////        execvp(&myargv[0], myargv);
+////        strtok()
+//
+////        close(0);
+////        dup(pfd(0));
+//        close(pfd(0));
+//        close(pfd(1));
+
+
+
+
+
+
 return 0;
 }
